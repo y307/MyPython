6a834ac2890babf2313dde059344bcd002c5d3bc
y307 <y307@ukr.net>
2017-11-06 09:25:14 +0200

    @work

--- vcard/vcards_w.py
+++ vcard/vcards_w.py
@@ -42,19 +42,20 @@ def get_new_filename(a_line):
 
 
 def do_file():
+    str_ext = '.vcf'
     file_name = entry_path.get()
     list_paths = os.path.split(file_name)
     num = 1
-    with open(file_name, 'r') as fin:
-        for line in fin.readlines():
+    with open(file_name, 'r') as f_in:
+        for line in f_in.readlines():
             # print(line)
             if line == 'BEGIN:VCARD\n':  # начало блока
-                t_name = 't'+str(num)+'.vcf'
+                t_name = 't'+str(num)+str_ext
                 new_path = os.path.join(list_paths[0], t_name)
                 new_name = ''
                 f_out = open(new_path, 'w')
                 f_out.write(line)
-                for line_next in fin.readline():
+                for line_next in f_in.readline():
                     # list_buf = line_next.split(line_next, ';')
                     if line_next[0:2] == 'N;':  # строка с именем
                         res = line_next.find(':=', 0)
@@ -69,7 +70,7 @@ def do_file():
                 if new_name == '':
                     num += 1
                 else:
-                    rename(new_path, os.path.join(folder, new_name+'.vcf'))
+                    rename(new_path, os.path.join(list_paths[0], new_name+str_ext))
 # do_file
 
 
