11a450620bbe480e4b341e5fd32448f78c2e0e6c
aly <y307@ukr.net>
2018-03-04 21:46:15 +0200

    20180304@home

--- Base64/base_64.py
+++ Base64/base_64.py
@@ -5,25 +5,38 @@ import base64
 import sys
 
 # разбор аргументов коммандной строки
-if __name__=="__main__":
-    if len(sys.argv) != 2:
-        print("Введите команду в формате: base_64.py <путь_к_файлу>")
-        sys.exit(1)
-    else:
-        fin_name = sys.argv[1]
+#if __name__=="__main__":
+#    if len(sys.argv) != 3:
+#        print("Введите команду в формате: base_64.py -[e|d] <путь_к_файлу>")
+#        sys.exit(1)
+#    else:
+        #mode = sys.argv[1]
+        #fin_name = sys.argv[2]
+mode = "-d"
+fin_name = "in.txt"
 
 fin = open(fin_name)
-# read text from file
-encodeText = fin.read()
-# convert text to bytes
-byte_encode=encodeText.encode()  # default parameter 'utf8'
-encodedText = base64.b64encode(byte_encode)  # encode text
-print(encodedText)
-
-decodedText = base64.b64decode(encodedText)  # decode encoded text
-print(decodedText)
 fout = open("out.txt", "wb")
-fout.write(decodedText)  # write decoded text to file
+
+if mode == '-e':
+    # read text from file
+    encodeText = fin.read()
+    # convert text to bytes
+    byte_encode=encodeText.encode()  # default parameter 'utf8'
+    # encode text
+    encodedText = base64.b64encode(byte_encode)
+    print(encodedText)
+    # write encoded text to file
+    fout.write(encodedText)
+elif mode == '-d':
+    decodeText = fin.read()
+    # decode encoded text
+    decodedText = base64.b64decode(decodeText)
+    ttt = str(decodedText, '')
+    print(ttt)
+    # write decoded text to file
+    # fout.write(str(decodedText).encode('latin1'))
+
 # close opened files
 fout.close()
 fin.close()
