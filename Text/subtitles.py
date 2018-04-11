c46bac9b79ae150f1557f02b35a9d4b5b4a52f28
aly <y307@ukr.net>
2018-04-11 16:57:19 +0300

    20180411@work

--- Text/subtitles.py
+++ Text/subtitles.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import time
 
 fin = open('in.srt')
 fout = open('out.srt', 'wt')
@@ -16,12 +17,20 @@ def shift_lines():
     buf_lines[3] = ''
 
 
-def format_time(astr=''):
+def time2sec(astr=''):
     time_list = astr.split(':')
-    time_str = time_list[0].zfill(2) + ':' + time_list[1].zfill(2)
-    if len(time_list) == 2:
-        time_str = '00:' + time_str
-    return time_str
+    l = len(time_list)
+    if l == 2:
+        return int(time_list[0]) * 60 + int(time_list[1])
+    elif l == 3:
+        return int(time_list[0]) * 3600 + int(time_list[1]) * 60 + int(time_list[2])
+
+
+def sec2time_str(asec):
+    hh = int(asec / 3600)
+    mm = int(asec % 3600 / 60)
+    ss = asec % 3600 % 60
+    return '{0:02d}:{1:02d}:{2:02.3f}'.format(hh, mm, ss)
 
 
 for line in fin:
@@ -32,12 +41,18 @@ for line in fin:
         if buf_lines[0] != '':
             # for i in range(4):
             #    buf_lines[i]=buf_lines[i].replace('\n', '')
-            time_start = format_time(buf_lines[1])
-            time_stop = format_time(buf_lines[3])
-            fout.write(str(num) + '\n')
-            fout.write(time_start + ' --> ' + time_stop + '\n')
-            fout.write(buf_lines[0] + '\n\n')
-        ########################################
+            time_start = time2sec(buf_lines[1])
+            time_stop = time2sec(buf_lines[3])
+            time_diff = time_stop - time_start
+            if time_diff > 60:
+                time_stop = time_start + 60
+            else:
+                time_stop -= 0.5
+            fout.write('{0:d}\n{1:s} --> {2:s}\n{3:s}\n\n'.format(num,
+                                                                  sec2time_str(time_start),
+                                                                  sec2time_str(time_stop),
+                                                                  buf_lines[0]))
+        #############################################################################
         shift_lines()
 
     else:
--- Text/test.py
+++ Text/test.py
@@ -1,3 +1,27 @@
 
-for x in range(4):
-    print(x)
+# Time
+import time
+
+t1=time.strptime('1:20', '%M:%S')
+t2=time.strptime('1:50', '%M:%S')
+t3=time.mktime(t2)-time.mktime(t1)
+print(t1)
+print(t2)
+print(t3)
+
+print(time.mktime(t2))
+print(time.mktime(t1))
+print(time.localtime(t3))
+print('\n')
+
+# String
+print('String\n')
+intg=1
+s='{0:02d}'.format(intg)
+print(s)
+flt=0.5
+s='{0:02.3f}'.format(flt)
+print(s)
+
+print('  ')
+print('OK')
