7f4a3a4c20ff8b6b9214213a7efe65e4b4a8cca0
aly <y307@ukr.net>
2018-04-12 16:59:16 +0300

    20180412@work

---
+++ Net/vidio_01.py
@@ -0,0 +1,5 @@
+import urllib.request
+
+dwn_link = 'https://class.coursera.org/textanalytics-001/lecture/download.mp4?lecture_id=73'
+file_name = 'trial_video.mp4'
+urllib.request.urlretrieve(dwn_link, file_name)
