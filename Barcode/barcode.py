8621a0337999580670b73de3ad28dd1aede7ef3d
aly <y307@ukr.net>
2018-05-08 15:18:51 +0300

    20180508@work

--- Barcode/barcode.py
+++ Barcode/barcode.py
@@ -11,7 +11,7 @@ def check_digit(aCode):
 
     while pos >= 0:
         # четные позиции умножаются на 3
-        summ += int(aCode[pos:pos+1]) * factor
+        summ += int(aCode[pos]) * factor
         # смена множителя на 1 или 3 при каждой итерации
         factor = 4 - factor
         # позиция следующей цифры
@@ -23,8 +23,53 @@ def check_digit(aCode):
 
     return res
 
+
 def coding(aCode):
+    list_A = ('0001101', '0011001', '0010011', '0111101', '0100011',
+              '0110001', '0101111', '0111011', '0110111', '0001011')
+    list_B = ('0100111', '0110011', '0011011', '0100001', '0011101',
+              '0111001', '0000101', '0010001', '0001001', '0010111')
+    list_C = ('1110010', '1100110', '1101100', '1000010', '1011100',
+              '1001110', '1010000', '1000100', '1001000', '1110100')
+    list_AB = ('AAAAAA', 'AABABB', 'AABBAB', 'AABBBA', 'ABAABB',
+               'ABBAAB', 'ABBBAA', 'ABABAB', 'ABABBA', 'ABBABA')
+
+    code_full = '101'
+    code_of_digit = ''
+
+    # комбинация для цифр 2-7
+    code_AB = list_AB[int(aCode[0])]
+
+    for pos in range(1, 13):
+        # текущуя цифра
+        digit = int(aCode[pos])
+
+        # выбор списка кодирования
+        if pos in range(1, 6):
+            ch_of_code = code_AB[pos - 1]
+        else:
+            ch_of_code = 'C'
+
+        # дополнение до полного кода
+        if ch_of_code == 'A':
+            code_of_digit = list_A[digit]
+        elif ch_of_code == 'B':
+            code_of_digit = list_B[digit]
+        elif ch_of_code == 'C':
+            code_of_digit = list_C[digit]
+
+        if pos == 7:
+            code_full += '10101'
+
+        code_full += code_of_digit
+    # end of for
+
+    return code_full + '101'
+############################################
 
 
+text = coding('2999300019561')
 
-print(check_digit('5070600050015'))
+print(text)
+
+print(check_digit('2999300019561'))
