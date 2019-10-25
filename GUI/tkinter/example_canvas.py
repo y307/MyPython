c6961b843349aba895db6931e8df35465bbe7a5c
y307 <y307@ukr.net>
2019-10-25 04:56:23 +0300

    Examples

--- GUI/tkinter/example_canvas.py
+++ GUI/tkinter/example_canvas.py
@@ -15,28 +15,24 @@ c.pack()
 
 c.create_line(10, 10, 50, 60)
 
-c.create_line(50, 160, 50, 80, fill='green',
-              width=5, arrow=LAST, dash=(10, 2),
-              activefill='lightgreen',
-              arrowshape="10 20 10")
+c.create_line(50, 160, 50, 80, fill='green', width=5, arrow=LAST, dash=(10, 2),
+              activefill='lightgreen', arrowshape="10 20 10")
 
 c.create_rectangle(80, 10, 120, 60)
 
-c.create_rectangle(80, 80, 120, 160, fill='yellow', outline='green',
-                   width=3, activedash=(5, 4))
+c.create_rectangle(80, 80, 120, 160, fill='yellow', outline='green', width=3, activedash=(5, 4))
 
 # треугольник - вершина, левый угол, правый угол
 c.create_polygon(220, 10, 160, 60, 280, 60)
 # трапеция
-c.create_polygon(190, 80, 250, 80, 280, 160, 160, 160,
-                 fill='orange', outline='black')
+c.create_polygon(190, 80, 250, 80, 280, 160, 160, 160, fill='orange', outline='black')
 
 # круг
 c.create_oval(330, 10, 380, 60, width=2)
 # овал
-c.create_oval(300, 100, 410, 160, fill='grey70', outline='white')
+c.create_oval(300, 100, 410, 160, fill='grey70', outline='black')
 # круг
-c.create_oval(450, 10, 600, 160, fill='lightgrey', outline='white')
+c.create_oval(450, 10, 600, 160, fill='lightgrey', outline='black')
 # сектор
 c.create_arc(450, 10, 600, 160, start=0, extent=45, fill='red')
 # сектор
